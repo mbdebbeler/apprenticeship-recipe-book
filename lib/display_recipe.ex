ea9cd0256d4b9cfb202fd863321f64a7fe60e41e
Monica Debbeler <m.b.debbeler@gmail.com>
2019-09-20 11:56:29 -0400

    add validation for user input

--- lib/display_recipe.ex
+++ lib/display_recipe.ex
@@ -1,10 +1,15 @@
 defmodule DisplayRecipe.CLI do
   use Application
+  require IEx
 
   def start(_type, _args) do
     children = []
     Supervisor.start_link(children, strategy: :one_for_one)
   end
+
+  @help_menu %{
+    "test" => "test"
+  }
 
   @outgoing_messages %{
     :welcome_screen => "Welcome to Recipe Book!"
@@ -16,7 +21,20 @@ defmodule DisplayRecipe.CLI do
 
   def print(text) do
     IO.puts(text)
-  end 
+  end
+
+  def receive_command do
+    user_input = IO.gets("") |> String.trim |> String.downcase
+    valid?(user_input)
+  end
+
+  def valid?(user_input) do
+    if @help_menu[user_input] do
+        print(@help_menu[user_input])
+    else
+        print("Invalid command. I don't know what to do.")
+    end
+  end
 
 end
 
--- test/display_recipe_test.exs
+++ test/display_recipe_test.exs
@@ -9,4 +9,15 @@ defmodule DisplayRecipeTest do
   test "#main prints a welcome message" do
     assert capture_io(fn -> DisplayRecipe.CLI.main end) == "Welcome to Recipe Book!\n"
   end
+
+  test "#receive_command returns a sanitized string when given valid input" do
+    assert capture_io(" tEsT  ", fn -> IO.write DisplayRecipe.CLI.receive_command end) == "test"
+  end
+
+  test "#receive_command prints failure message when given invalid input" do
+    assert capture_io("/n", fn -> IO.write DisplayRecipe.CLI.receive_command end) == "Invalid command. I don't know what to do."
+    assert capture_io("@1234685436!!!!", fn -> IO.write DisplayRecipe.CLI.receive_command end) == "Invalid command. I don't know what to do."
+    assert capture_io("               ", fn -> IO.write DisplayRecipe.CLI.receive_command end) == "Invalid command. I don't know what to do."
+  end
+
 end
