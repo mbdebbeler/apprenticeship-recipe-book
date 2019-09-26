e8063ea8dad9162939a1db4902ac2bd400deac3c
Monica Debbeler <m.b.debbeler@gmail.com>
2019-09-26 17:16:25 -0400

    WIP: parsing out items before Instruction section

--- lib/display_recipe.ex
+++ lib/display_recipe.ex
@@ -33,18 +33,36 @@ defmodule DisplayRecipe.CLI do
   end
 
   def find_ingredients(recipe_lines) do
-    find_ingredients(recipe_lines, [], false)
+    find_ingredients(recipe_lines, [])
   end
 
-  def find_ingredients([recipe_line | remaining_lines], ingredients_list, collect) do
+  def find_ingredients([recipe_line | remaining_lines], ingredients_list) do
     case recipe_line do
       "Ingredients:" ->
-        find_ingredients(remaining_lines, ingredients_list, true)
+        is_before_next_section?(remaining_lines)
 
       _ ->
-        find_ingredients(remaining_lines, ingredients_list, collect)
+        find_ingredients(remaining_lines, ingredients_list)
     end
   end
 
-  def find_ingredients([], ingredients_list, _), do: ingredients_list
+  def find_ingredients([], ingredients_list), do: ingredients_list
+
+  def is_before_next_section?(remaining_lines) do
+    is_before_next_section?(remaining_lines, [])
+  end
+
+  def is_before_next_section?([recipe_line | remaining_lines], instruction_list) do
+    print remaining_lines
+    print recipe_line
+    case recipe_line do
+      "bab" ->
+        print ~s(#{recipe_line})
+
+      _ ->
+        print ~s(I am not recipes)
+
+    end
+  end
+
 end
