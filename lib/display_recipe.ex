d903363084ff54d810c4fa000ee44b842a44e5e8
Monica Debbeler <m.b.debbeler@gmail.com>
2019-09-26 13:14:59 -0400

    Add methods to print contents of read files

--- lib/display_recipe.ex
+++ lib/display_recipe.ex
@@ -1,10 +1,14 @@
 defmodule DisplayRecipe.CLI do
-  @outgoing_messages %{
+  @user_prompts %{
     :welcome_screen => "Welcome to Recipe Book!"
   }
 
+  @recipe_files %{
+    :ice_cubes => "../recipes/ice_cubes.txt"
+  }
+
   def main(_args) do
-    print(@outgoing_messages[:welcome_screen])
+    print(@user_prompts[:welcome_screen])
   end
 
   def print(text) do
@@ -13,6 +17,14 @@ defmodule DisplayRecipe.CLI do
 
   def read_file(filepath) do
     File.read!(filepath)
+  end
+
+  def print_file_contents do
+    print(read_file(@recipe_files[:ice_cubes]))
+  end 
+
+  def print_file_contents(filepath) do
+    print(read_file(filepath))
   end
 
 end
--- test/display_recipe_test.exs
+++ test/display_recipe_test.exs
@@ -12,10 +12,29 @@ defmodule DisplayRecipeTest do
   end
 
   describe "read_file/1" do
-  	test "accepts string filepath and returns string of file contents" do
-		example_filepath = "../recipes/ice_cubes.txt"
-		assert read_file(example_filepath) == "Title:\nIce Cubes\n\nIngredients:\n2 cups water (approximately)\n2 tablespoons water (additional if needed)\n\nDirections:\n- Empty any ice cubes that are left in the trays into the bin.\n- Take the trays over to the sink and fill them with water. (Tip: hot water will freeze faster and the cubes will be more clear.)\n- Place the water-filled ice trays back in the freezer.\n- Replace the ice bin if you had to remove it.\n- Shut the door to the freezer.\n- Be sure to leave for around 4-6 hours at least to make sure it is frozen.\n"
-	end 
-  end 
+    test "accepts string filepath and returns string of file contents" do
+      example_filepath = "../recipes/ice_cubes.txt"
 
+      assert read_file(example_filepath) ==
+               "Title:\nIce Cubes\n\nIngredients:\n2 cups water (approximately)\n2 tablespoons water (additional if needed)\n\nDirections:\n- Empty any ice cubes that are left in the trays into the bin.\n- Take the trays over to the sink and fill them with water. (Tip: hot water will freeze faster and the cubes will be more clear.)\n- Place the water-filled ice trays back in the freezer.\n- Replace the ice bin if you had to remove it.\n- Shut the door to the freezer.\n- Be sure to leave for around 4-6 hours at least to make sure it is frozen.\n"
+    end
+  end
+
+  describe "print_file_contents/0" do
+    test "prints a string of a hardcoded filepath's contents" do
+      assert capture_io(fn -> print_file_contents() end) ==
+               "Title:\nIce Cubes\n\nIngredients:\n2 cups water (approximately)\n2 tablespoons water (additional if needed)\n\nDirections:\n- Empty any ice cubes that are left in the trays into the bin.\n- Take the trays over to the sink and fill them with water. (Tip: hot water will freeze faster and the cubes will be more clear.)\n- Place the water-filled ice trays back in the freezer.\n- Replace the ice bin if you had to remove it.\n- Shut the door to the freezer.\n- Be sure to leave for around 4-6 hours at least to make sure it is frozen.\n" <>
+                 "\n"
+    end
+  end
+
+  describe "print_file_contents/1" do
+    test "accepts string filepath and prints string of file contents" do
+      example_filepath = "../recipes/ice_cubes.txt"
+
+      assert capture_io(fn -> print_file_contents(example_filepath) end) ==
+               "Title:\nIce Cubes\n\nIngredients:\n2 cups water (approximately)\n2 tablespoons water (additional if needed)\n\nDirections:\n- Empty any ice cubes that are left in the trays into the bin.\n- Take the trays over to the sink and fill them with water. (Tip: hot water will freeze faster and the cubes will be more clear.)\n- Place the water-filled ice trays back in the freezer.\n- Replace the ice bin if you had to remove it.\n- Shut the door to the freezer.\n- Be sure to leave for around 4-6 hours at least to make sure it is frozen.\n" <>
+                 "\n"
+    end
+  end
 end
