880eddfedcc889362e55233ee2925cac55a7c8ea
Monica Debbeler <m.b.debbeler@gmail.com>
2019-10-17 17:13:17 -0400

    Handle indefinite articles and non-number quantity words

--- lib/word_to_number.ex
+++ lib/word_to_number.ex
@@ -1,12 +1,21 @@
 defmodule WordToNumber do
 
-  def convert(number) do
-    to_number(String.downcase(number))
+  def convert_indefinite(word_list) do
+    Enum.chunk_every(word_list, 2)
+    |> Enum.map(fn chunk -> indefinite_to_number(chunk) end)
   end
 
+  def convert(number) do
+    to_number(number)
+  end
+
+  defp indefinite_to_number(["a", "dozen"]), do: 12
+  defp indefinite_to_number(["a", "couple"]), do: 2
+  defp indefinite_to_number(["a", _]), do: 1
+  defp indefinite_to_number(["an", _]), do: 1
+  defp indefinite_to_number(chunk), do: chunk
+
   defp to_number("one"), do: 1
-  defp to_number("a"), do: 1
-  defp to_number("an"), do: 1
   defp to_number("two" ),  do: 2
   defp to_number("three" ),  do: 3
   defp to_number("four" ),  do: 4
@@ -26,6 +35,6 @@ defmodule WordToNumber do
   defp to_number("eighteen" ), do: 18
   defp to_number("nineteen" ), do: 19
   defp to_number("twenty" ), do: 20
-  defp to_number(_), do: nil
+  defp to_number(number), do: number
 
 end
