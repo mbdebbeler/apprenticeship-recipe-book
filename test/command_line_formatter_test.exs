94b92a9c4f69b44dd3e1328a7e19bce5f18bf419
Monica Debbeler <m.b.debbeler@gmail.com>
2019-10-30 00:07:55 -0400

    Add parse_ingredients/1 and helper functions to RecipeParser

--- test/command_line_formatter_test.exs
+++ test/command_line_formatter_test.exs
@@ -3,8 +3,70 @@ defmodule CommandLineFormatterTest do
   import CommandLineFormatter
 
   describe "format_recipe/1" do
-    test "takes a %Recipe{} struct and returns a string with formatted title, servings, ingredients and directions"
-  end 
-  end
+    test "takes a %Recipe{} struct and returns a string with formatted title, servings, ingredients and directions" do
+      example_struct = %Recipe{
+        directions: [
+          %{
+            direction:
+              "Do not substitute smaller French lentils for the green or brown lentils. When preparing the Crispy Onions (see related content), be sure to reserve  tablespoons of the onion cooking oil for cooking the rice and lentils.",
+            display_index: "Before you start: "
+          },
+          %{
+            direction:
+              "FOR THE YOGURT SAUCE: Whisk all ingredients together in bowl. Refrigerate while preparing rice and lentils.",
+            display_index: 1
+          },
+          %{
+            direction:
+              "FOR THE RICE AND LENTILS: Bring lentils,  cups water, and  teaspoon salt to boil in medium saucepan over high heat. Reduce heat to low and cook until lentils are tender,  to  minutes. Drain and set aside. While lentils cook, place rice in medium bowl and cover by  inches with hot tap water; let stand for  minutes.",
+            display_index: 2
+          },
+          %{
+            direction:
+              "Using your hands, gently swish rice grains to release excess starch. Carefully pour off water, leaving rice in bowl. Add cold tap water to rice and pour off water. Repeat adding and pouring off cold tap water  to  times, until water runs almost clear. Drain rice in fine-mesh strainer.",
+            display_index: 3
+          },
+          %{
+            direction:
+              "Heat reserved onion oil, garlic, coriander, cumin, cinnamon, allspice, 1/4 teaspoon pepper, and cayenne in Dutch oven over medium heat until fragrant, about  minutes. Add rice and cook, stirring occasionally, until edges of rice begin to turn translucent, about  minutes. Add  1/4 cups water, sugar, and  teaspoon salt and bring to boil. Stir in lentils, reduce heat to low, cover, and cook until all liquid is absorbed, about  minutes.",
+            display_index: 4
+          },
+          %{
+            direction:
+              "Off heat, remove lid, fold dish towel in half, and place over pot; replace lid. Let stand for  minutes. Fluff rice and lentils with fork and stir in cilantro and half of crispy onions. Transfer to serving platter, top with remaining crispy onions, and serve, passing yogurt sauce separately.",
+            display_index: 5
+          }
+        ],
+        ingredients: [
+          "YOGURT SAUCE",
+          "1 cup plain whole-milk yogurt",
+          "2 tablespoons lemon juice",
+          "1/2 teaspoon minced garlic",
+          "1/2 teaspoon salt",
+          "RICE AND LENTILS",
+          "8 1/2 ounces (1 1/4 cups) green or brown lentils, picked over and rinsed",
+          "Salt and pepper",
+          "1 1/4 cups basmati rice",
+          "2 cups Crispy Onions",
+          "3 garlic cloves, minced",
+          "1 teaspoon ground coriander",
+          "1 teaspoon ground cumin",
+          "1/2 teaspoon ground cinnamon",
+          "1/2 teaspoon ground allspice",
+          "1/8 teaspoon cayenne pepper",
+          "1 teaspoon sugar",
+          "3 tablespoons minced fresh cilantro"
+        ],
+        servings: %{max: 6, min: 4},
+        title: "Rice and Lentils with Crispy Onions (Mujaddara)"
+      }
 
+      expected_output =
+        "Serves: 4 to 6\n\nIngredients:\nYOGURT SAUCE\n1 cup plain whole-milk yogurt"
+
+      output = format_recipe(example_struct)
+
+      assert String.contains?(output, expected_output)
+    end
+  end
 end
--- test/recipe_parser_test.exs
+++ test/recipe_parser_test.exs
@@ -6,9 +6,11 @@ defmodule RecipeParserTest do
     test "returns a %Recipe{} struct with title, servings, ingredients and directions" do
       filepath = './recipes/mujaddara.txt'
       output = parse_tokens(filepath)
+
       first_expected_direction = %{
-        direction: "Do not substitute smaller French lentils for the green or brown lentils. When preparing the Crispy Onions (see related content), be sure to reserve  tablespoons of the onion cooking oil for cooking the rice and lentils.",
-        display_index: "Before you start: "
+        direction:
+          "Do not substitute smaller French lentils for the green or brown lentils. When preparing the Crispy Onions (see related content), be sure to reserve 3 tablespoons of the onion cooking oil for cooking the rice and lentils.",
+        display_index: "Before you start"
       }
 
       assert %{title: "Rice and Lentils with Crispy Onions (Mujaddara)"} = output
@@ -18,51 +20,54 @@ defmodule RecipeParserTest do
   end
 
   describe "parse_ingredients/1" do
-     test "when there are sublists, returns a List of ingredients with subrecipe titles included in list" do
-       filepath = './recipes/mujaddara.txt'
-       tokens = Parser.lex(read_file(filepath))
+    test "when there are sublists, returns a List of ingredients with subrecipe titles included in list" do
+      filepath = './recipes/mujaddara.txt'
+      tokens = Parser.lex(read_file(filepath))
 
-       output = parse_ingredients(tokens)
-       first_expected_ingredient = "YOGURT SAUCE"
+      output = parse_ingredients(tokens)
+      first_expected_ingredient = "YOGURT SAUCE"
 
-       assert Enum.member?(output, first_expected_ingredient)
-     end
+      assert Enum.member?(output, first_expected_ingredient)
+    end
 
-     test "when there are no sublists, returns a List of ingredients" do
-       filepath = './recipes/esquites.txt'
-       tokens = Parser.lex(read_file(filepath))
+    test "when there are no sublists, returns a List of ingredients" do
+      filepath = './recipes/esquites.txt'
+      tokens = Parser.lex(read_file(filepath))
 
-       output = parse_ingredients(tokens)
-       first_expected_ingredient = "3 tablespoons lime juice, plus extra for seasoning (2 limes)"
+      output = parse_ingredients(tokens)
+      first_expected_ingredient = "3 tablespoons lime juice, plus extra for seasoning (2 limes)"
 
-       assert Enum.member?(output, first_expected_ingredient)
-     end
+      assert Enum.member?(output, first_expected_ingredient)
+    end
   end
 
   describe "parse_directions/1" do
-     test "returns a List of directions, each stored in a map that contains :display_index and :direction" do
-       filepath = './recipes/mujaddara.txt'
-       tokens = Parser.lex(read_file(filepath))
+    test "returns a List of directions, each stored in a map that contains :display_index and :direction" do
+      filepath = './recipes/mujaddara.txt'
+      tokens = Parser.lex(read_file(filepath))
 
-       output = parse_directions(tokens)
+      output = parse_directions(tokens)
 
-       first_expected_direction = %{
-                direction: "Do not substitute smaller French lentils for the green or brown lentils. When preparing the Crispy Onions (see related content), be sure to reserve  tablespoons of the onion cooking oil for cooking the rice and lentils.",
-                display_index: "Before you start: "
-              }
-        second_expected_direction = %{
-                direction: "FOR THE YOGURT SAUCE: Whisk all ingredients together in bowl. Refrigerate while preparing rice and lentils.",
-                display_index: 1
-              }
+      first_expected_direction = %{
+        direction:
+          "Do not substitute smaller French lentils for the green or brown lentils. When preparing the Crispy Onions (see related content), be sure to reserve 3 tablespoons of the onion cooking oil for cooking the rice and lentils.",
+        display_index: "Before you start"
+      }
 
-       assert Enum.member?(output, first_expected_direction)
-       assert Enum.member?(output, second_expected_direction)
-     end
+      second_expected_direction = %{
+        direction:
+          "FOR THE YOGURT SAUCE: Whisk all ingredients together in bowl. Refrigerate while preparing rice and lentils.",
+        display_index: 1
+      }
+
+      assert Enum.member?(output, first_expected_direction)
+      assert Enum.member?(output, second_expected_direction)
+    end
   end
 
   describe "parse_servings/2" do
     test "returns a Map of servings for each recipe with :min and :max fields" do
-            tokens = [
+      tokens = [
         {:word, 1, 'Rice'},
         {:whitespace, 1, ' '},
         {:word, 1, 'and'},
@@ -204,173 +209,6 @@ defmodule RecipeParserTest do
       output = read_file(filepath)
 
       assert output == expected_output
-    end
-  end
-
-  describe "split_file_by_lines/1" do
-    test "when passed a string of file contents, it splits the string by newlines and returns an list of strings" do
-      example_recipe_chunk = "foo\nbar\nbaz"
-
-      assert split_file_by_lines(example_recipe_chunk) == ["foo", "bar", "baz"]
-    end
-  end
-
-  describe "is_after_ingredients/1" do
-    test "when passed a list of strings, it will return a sub-list of strings between 'ingredients' and a blank line" do
-      example_recipe_chunk = [
-        "foo",
-        "bar",
-        "",
-        "INGREDIENTS",
-        "baz",
-        "zab",
-        "",
-        "Recipes:",
-        "bab"
-      ]
-
-      assert is_after_ingredients(example_recipe_chunk) == ["baz", "zab", "", "Recipes:", "bab"]
-    end
-  end
-
-  describe "is_before_section_break/1" do
-    test "when passed a list of strings, it will return a sub-list of strings before the blank line" do
-      example_recipe_chunk = ["baz", "zab", "", "Recipes:", "bab"]
-
-      assert is_before_section_break(example_recipe_chunk) == ["baz", "zab"]
-    end
-  end
-
-  describe "split_line_by_words/1" do
-    test "when passed a string, splits it by spaces" do
-      ingredients_line = "4 large dried New Mexico or guajillo chiles, stemmed, halved, seeded"
-
-      split_ingredients_line = [
-        "4",
-        "large",
-        "dried",
-        "New",
-        "Mexico",
-        "or",
-        "guajillo",
-        "chiles,",
-        "stemmed,",
-        "halved,",
-        "seeded"
-      ]
-
-      result = split_line_by_words(ingredients_line)
-
-      assert result == split_ingredients_line
-    end
-  end
-
-  describe "transform_line/2" do
-    test "when passed a list of strings and desired_servings, filters and multiplies integers by desired_servings, returns a list of strings" do
-      split_ingredients_line = [
-        "4",
-        "large",
-        "dried",
-        "New",
-        "Mexico",
-        "or",
-        "guajillo",
-        "chiles,",
-        "stemmed,",
-        "halved,",
-        "seeded"
-      ]
-
-      processed_ingredients_line = [
-        "8",
-        "large",
-        "dried",
-        "New",
-        "Mexico",
-        "or",
-        "guajillo",
-        "chiles,",
-        "stemmed,",
-        "halved,",
-        "seeded"
-      ]
-
-      desired_servings = 2
-
-      assert transform_line(split_ingredients_line, desired_servings) ==
-               processed_ingredients_line
-    end
-  end
-
-  describe "is_valid_quantity/1" do
-    test "returns true when passed a string that respresents an integer" do
-      string_four = "4"
-
-      assert is_valid_quantity(string_four) == true
-    end
-
-    test "returns false when passed -1" do
-      string_neg_one = "-1"
-
-      assert is_valid_quantity(string_neg_one) == false
-    end
-
-    test "returns false when passed 0" do
-      string_zero = "0"
-
-      assert is_valid_quantity(string_zero) == false
-    end
-  end
-
-  describe "join_line_by_words/1" do
-    test "concatenates a list of strings into one string" do
-      ingredients_list = [
-        "4",
-        "large",
-        "dried",
-        "New",
-        "Mexico",
-        "or",
-        "guajillo",
-        "chiles,",
-        "stemmed,",
-        "halved,",
-        "seeded"
-      ]
-
-      ingredients_line = "4 large dried New Mexico or guajillo chiles, stemmed, halved, seeded"
-
-      assert join_line_by_words(ingredients_list) == ingredients_line
-    end
-  end
-
-  describe "change_servings/1" do
-    test "takes a context and returns an updated context" do
-      example_context = %{
-        input: "3",
-        content: ["5 spicy olives", "20 potato chips"],
-        header: nil,
-        view: :generate_grocery_list,
-        io: IO,
-        prompt: "Foo",
-        menu: "Bar",
-        error: nil,
-        last_input: "1"
-      }
-
-      transformed_context = %{
-        input: "3",
-        content: ["15 spicy olives", "60 potato chips"],
-        header: nil,
-        view: :generate_grocery_list,
-        io: IO,
-        prompt: "Foo",
-        menu: "Bar",
-        error: nil,
-        last_input: "3"
-      }
-
-      assert change_servings(example_context) == transformed_context
     end
   end
 
