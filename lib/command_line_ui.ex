e40580750be0bfd05e72faf48f2fb4c087a846e9
Monica Debbeler <m.b.debbeler@gmail.com>
2019-11-01 11:00:16 -0400

    Move string manipulation to sanitize_input/1, handle typed recipe titles

--- lib/command_line_ui.ex
+++ lib/command_line_ui.ex
@@ -17,12 +17,44 @@ defmodule CommandLineUI do
     context
   end
 
-  defp sanitize_input(nil, _io) do
+  def sanitize_input(nil, _io) do
     :invalid_input
   end
 
-  defp sanitize_input(message, _io) do
-    String.capitalize(message)
+  def sanitize_input(message, _io) do
+    if is_input_a_recipe?(message) do
+
+    end
+
+    recipes =
+      Parser.prepare_recipe_index_map()
+      |> Map.keys()
+      |> Enum.with_index(1)
+      |> Enum.into(%{})
+
+
+# needs to handle Q, quit, I, Grocery list, G
+  end
+
+  defp fetch_recipe(message) do
+    if Enum.member?(Map.values(recipes), message) do
+      message
+    else
+      if Map.has_key?(recipes, message) do
+        Map.fetch!(recipes, message)
+      else
+        :invalid_input
+      end
+    end
+  end
+
+  defp is_input_a_recipe?(message) do
+    recipes =
+      Parser.prepare_recipe_index_map()
+      |> Map.keys()
+      |> Enum.with_index(1)
+      |> Enum.into(%{})
+    Enum.member?(Map.values(recipes), message) || Map.has_key?(recipes, message)
   end
 
   defp clear_display(context, io) do
